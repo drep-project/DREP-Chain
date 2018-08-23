9fc04b63bacc9aad7c0440f3e1b06d5e2f70a548
hejiawei <jiaweihe@drep.com>
2018-08-23 21:09:44 +0800

    Spread方法添加

--- network/Peer.go
+++ network/Peer.go
@@ -5,32 +5,33 @@ import (
     "errors"
     "time"
     "sync"
+    "BlockChainTest/crypto"
+    "BlockChainTest/common"
 )
 
-var local = "127.0.0.1"
-var curve = InitCurve()
-var key *PrivateKey
-var once0, once1 sync.Once
+//var local = "127.0.0.1"
+//var curve = crypto.InitCurve()
+//var key *common.PrivateKey
+//var once0, once1 sync.Once
 
 type Peer struct {
     IP       string
     msg      interface{}
 }
 
-
 func (peer *Peer) String() string{
     return peer.IP + ":" + strconv.Itoa(peer.Port)
 }
 
-func GetPrvKey() *PrivateKey {
-   once1.Do(func() {
-       var prvKey *PrivateKey = nil
-       err := errors.New("fail to generate key pair")
-       for err != nil {
-           prvKey, err = GenerateKey(curve)
-           key = prvKey
-           time.Sleep(100 * time.Millisecond)
-       }
-   })
-   return key
-}
+//func GetPrvKey() *PrivateKey {
+//   once1.Do(func() {
+//       var prvKey *PrivateKey = nil
+//       err := errors.New("fail to generate key pair")
+//       for err != nil {
+//           prvKey, err = GenerateKey(curve)
+//           key = prvKey
+//           time.Sleep(100 * time.Millisecond)
+//       }
+//   })
+//   return key
+//}
