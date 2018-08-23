aaa044313e57197219e4f4fc39276271f2d43970
hejiawei <jiaweihe@drep.com>
2018-08-23 17:56:48 +0800

    sendMessage function reconstitution

--- network/P2PComm.go
+++ network/P2PComm.go
@@ -2,9 +2,14 @@ package network
 
 import (
     "fmt"
+    "BlockChainTest/common"
+    "net"
+    "sync"
 )
 //const START_BYTE_NORMAL = 0x11
 //const START_BYTE_BROADCAST = 0x22
+
+var once sync.Once
 
 type P2PComm struct {
     IPs  []string
@@ -22,39 +27,33 @@ func SharedP2pComm() *P2PComm {
     return sharedInstance
 }
 
-func (p *P2PComm) SendMessage(peers []*Peer, msg interface{})  {
+func (p *P2PComm) SendMessage(peers []*Peer, msg interface{}) error {
     for _, peer := range peers  {
         // 利用proto buffer序列化
-        _, error := Serialize(msg)
-        if error != nil{
-            println(error)
-            return
+        bytes, err := common.Serialize(msg)
+        if err != nil {
+            return err
         }
-        p.SendMessageCore(peer, msg)
+        p.SendMessageCore(peer, bytes)
     }
+    return nil
 }
 
-func (p *P2PComm) SendMessageCore(peer *Peer, msg interface{})  {
-    //func NewBroadcast(peer.Net.channel, port int, msg interface{}, que chan *Sender) *Broadcast {
-    //return &Broadcast{ips, peer.po, msg, que}
-    //}
-    leader := NewLeader(peer)
-    leader.Listen()
-    leader.Work()
+func (p *P2PComm) SendMessageCore(peer *Peer, bytes []byte) error {
 
-    // step 1
-    // leader request ticket
-    err := leader.RequestTicket()
+    addr, err := net.ResolveTCPAddr("tcp", peer.String())
     if err != nil {
-        fmt.Println("error: \n", err)
+        return err
     }
-
-    if b := leader.ValidateTicket(); b {
-        fmt.Println("leader validate ticket: ", b)
-        fmt.Println()
-    } else {
-        return
+    conn, err := net.DialTCP("tcp", nil, addr)
+    if err != nil {
+        return err
     }
+    defer conn.Close()
+    if _, err := conn.Write(bytes); err != nil {
+        return err
+    }
+    return nil
 }
 
 func (p *P2PComm) Spread() error {
