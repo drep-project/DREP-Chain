2c807ceb87e33962d6c6434ddf81cfafafbfa67d
Eric Chao <ericchao@drep.org>
2018-08-27 20:31:03 +0800

    temp

--- bean/common.go
+++ bean/common.go
@@ -1,3 +1,15 @@
 package bean
 
 type Address string
+
+const (
+    MsgTypeAccount     = 0
+    MsgTypeBlockHeader = 1
+    MsgTypeBlockData   = 2
+    MsgTypeBlock       = 3
+    MsgTypeTransaction = 4
+    MsgTypeSetUp       = 5
+    MsgTypeCommitment  = 6
+    MsgTypeChallenge   = 7
+    MsgTypeResponse    = 8
+)
