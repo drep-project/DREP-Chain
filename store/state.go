ec3feceea59e2ba598aadb3d81fa41aadcc12402
Eric Chao <ericchao@drep.org>
2018-08-29 23:26:19 +0800

    Write the go round

--- store/state.go
+++ store/state.go
@@ -6,18 +6,16 @@ import (
     "BlockChainTest/bean"
     "BlockChainTest/consensus"
     "BlockChainTest/crypto"
+    "BlockChainTest/log"
 )
-
-
-const index = 0
 
 var (
     role int
     leader *consensus.Leader
     member *consensus.Member
-    miningState int
+    //miningState int
     miners []*network.Peer
-    minerIndex map[bean.Address]int
+    //minerIndex map[bean.Address]int
     peers []*network.Peer
     blockHeight int
     block *bean.Block
@@ -25,17 +23,14 @@ var (
     prvKey *bean.PrivateKey
     pubKey *bean.Point
     address bean.Address
-    //k0, k1, k2 []byte
-    //pub0, pub1, pub2 *bean.Point
-    //prv0, prv1, prv2 *bean.PrivateKey
-    //miner0, miner1, miner2 *network.Peer
-    //ip0, ip1, ip2 network.IP
-    //port0, port1, port2 network.Port
-    //peer0, peer1, peer2 *network.Peer
+
+    currentMinerIndex int
+    myIndex int
 )
 
 func init()  {
     lock = &sync.Mutex{}
+    currentMinerIndex = -1
     //prvKey, _ = crypto.GetPrivateKey()
     //pubKey = GetPubKey()
     curve := crypto.GetCurve()
@@ -59,44 +54,50 @@ func init()  {
     peer2 := &network.Peer{IP: ip2, Port: port2, PubKey: pub2}
     miners = []*network.Peer{peer0, peer1, peer2}
     peers = []*network.Peer{peer0, peer1, peer2}
-    switch index {
+    switch myIndex {
     case 0:
         pubKey = pub0
         prvKey = prv0
-        leader = consensus.NewLeader(pub0, peers)
-        member = nil
+        //leader = consensus.NewLeader(pub0, peers)
+        //member = nil
     case 1:
         pubKey = pub1
         prvKey = prv1
-        leader = nil
-        member = consensus.NewMember(peer0, prvKey)
+        //leader = nil
+        //member = consensus.NewMember(peer0, prvKey)
     case 2:
         pubKey = pub2
         prvKey = prv2
-        leader = nil
-        member = consensus.NewMember(peer0, prvKey)
+        //leader = nil
+        //member = consensus.NewMember(peer0, prvKey)
     }
 }
 
-func ChangeRole(r int) {
+func ChangeRole() {
     lock.Lock()
-    role = r
     miners = GetMiners()
+    currentMinerIndex = (currentMinerIndex + 1) % len(miners)
+    log.Println("Current miner index:", currentMinerIndex, " my index: ", myIndex)
+    if currentMinerIndex == myIndex {
+        role = bean.LEADER
+    } else {
+        role = bean.MEMBER
+    }
+    leader = nil
+    member = nil
     lock.Unlock()
+}
+
+func SetLeader(l *consensus.Leader) {
+    leader = l
+}
+
+func SetMember(m *consensus.Member) {
+    member = m
 }
 
 func GetRole() int {
     return role
-}
-
-func ChangeMiningState(s int) {
-    lock.Lock()
-    miningState = s
-    lock.Unlock()
-}
-
-func GetMiningState() int {
-    return miningState
 }
 
 func GetMiners() []*network.Peer {
@@ -129,16 +130,8 @@ func GetBlockHeight() int {
     return blockHeight
 }
 
-func CheckState(r int, ms int) bool {
-    return role == r && miningState == ms
-}
-
 func CheckRole(r int) bool {
     return role == r
-}
-
-func MoveToState(ms int) {
-    miningState = ms
 }
 
 func GetLeader() *network.Peer {
@@ -171,14 +164,4 @@ func GetItSelfOnLeader() *consensus.Leader {
 
 func GetItSelfOnMember() *consensus.Member {
     return member
-}
-
-func GetMinerIndex(p *bean.Point) int {
-    if pubKey.Equal(p) {
-        return -1
-    }
-    if index, exist := minerIndex[p.Addr()]; exist {
-        return index
-    }
-    return -1
 }
