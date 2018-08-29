00fa3edf5b38523a2ad4df14139148bed4b94f03
Eric Chao <ericchao@drep.org>
2018-08-29 19:10:07 +0800

    temp

--- consensus/leader.go
+++ consensus/leader.go
@@ -7,9 +7,9 @@ import (
     "BlockChainTest/crypto"
     "math/big"
     "BlockChainTest/hash"
-    "fmt"
     "math"
     "BlockChainTest/store"
+    "BlockChainTest/log"
 )
 
 const (
@@ -55,28 +55,28 @@ func (l *Leader) ProcessConsensus(msg []byte) (*bean.Signature, []byte) {
     l.commitWg = sync.WaitGroup{}
     l.commitWg.Add(len(l.members))
     l.state = setUp
-    fmt.Println("Leader is going to setup")
+    log.Println("Leader is going to setup")
     l.setUp(msg, l.pubKey)
-    fmt.Println("Leader wait for commit")
+    log.Println("Leader wait for commit")
     l.commitWg.Wait()
 
     l.responseWg = sync.WaitGroup{}
     l.responseWg.Add(len(l.commitBitmap))
     l.state = challenge
-    fmt.Println("Leader is going to challenge")
+    log.Println("Leader is going to challenge")
     l.challenge(msg)
-    fmt.Println("Leader wait for response")
+    log.Println("Leader wait for response")
     l.responseWg.Wait()
-    fmt.Println("Leader finish")
+    log.Println("Leader finish")
     sig := &bean.Signature{R: l.r, S: l.sigmaS.Bytes()}
     valid := l.Validate(sig, msg)
-    fmt.Println("valid? ", valid)
+    log.Println("valid? ", valid)
     return sig, l.responseBitmap
 }
 
 func (l *Leader) setUp(msg []byte, pubKey *bean.Point) {
     setup := &bean.Setup{Msg: msg, PubKey: pubKey}
-    fmt.Println("Leader setup ", *setup)
+    log.Println("Leader setup ", *setup)
     network.SendMessage(l.members, setup)
 }
 
@@ -91,7 +91,7 @@ func (l *Leader) getR(msg []byte) []byte {
 func (l *Leader) challenge(msg []byte)  {
     l.r = l.getR(msg)
     challenge := &bean.Challenge{SigmaPubKey: l.sigmaPubKey, SigmaQ: l.sigmaQ, R: l.r}
-    fmt.Println("Leader challenge ", *challenge)
+    log.Println("Leader challenge ", *challenge)
     network.SendMessage(l.members, challenge)
 }
 
@@ -99,7 +99,7 @@ func isLegalIndex(index int, bitmap []byte) bool {
     return index >=0 && index <= len(bitmap) && bitmap[index] != 1
 }
 func (l *Leader) ProcessCommit(commit *bean.Commitment) {
-    fmt.Println("Leader process commit ", *commit)
+    log.Println("Leader process commit ", *commit)
     if l.state != setUp {
         return
     }
@@ -119,7 +119,7 @@ func (l *Leader) ProcessCommit(commit *bean.Commitment) {
 }
 
 func (l *Leader) ProcessResponse(response *bean.Response) {
-    fmt.Println("Leader process response ", *response)
+    log.Println("Leader process response ", *response)
     if l.state != challenge {
         return
     }
--- node/node.go
+++ node/node.go
@@ -5,6 +5,8 @@ import (
     "BlockChainTest/bean"
     "BlockChainTest/consensus"
     "github.com/golang/protobuf/proto"
+    "sync"
+    "BlockChainTest/log"
 )
 
 const (
@@ -21,6 +23,7 @@ type Node struct {
     role int
     address *bean.Address
     prvKey *bean.PrivateKey
+    wg *sync.WaitGroup
 }
 
 func NewNode(role int, prvKey *bean.PrivateKey) *Node {
@@ -38,6 +41,7 @@ func (n *Node) isLeader() bool {
 
 func (n *Node) Start() {
     for {
+        log.Println("node start")
         switch store.GetRole() {
         case LEADER:
             n.runAsLeader()
@@ -46,23 +50,45 @@ func (n *Node) Start() {
         case OTHER:
             n.runAsOther()
         }
+        log.Println("node stop")
     }
 }
 
 func (n *Node) runAsLeader() {
-    leader := consensus.NewLeader(n.prvKey.PubKey, store.GetMiners())
+    leader1 := consensus.NewLeader(n.prvKey.PubKey, store.GetMiners())
     block := store.GetBlock()
+    log.Println("node leader is preparing process consensus for round 1")
     if msg, err := proto.Marshal(block); err ==nil {
-        sig, bitmap := leader.ProcessConsensus(msg)
+        log.Println("node leader is going to process consensus for round 1")
+        sig, bitmap := leader1.ProcessConsensus(msg)
         multiSig := &bean.MultiSignature{Sig: sig, Bitmap: bitmap}
+        log.Println("node leader is preparing process consensus for round 2")
         if msg, err := proto.Marshal(multiSig); err == nil {
-            leader.ProcessConsensus(msg)
+            leader2 := consensus.NewLeader(n.prvKey.PubKey, store.GetMiners())
+            log.Println("node leader is going to process consensus for round 2")
+            leader2.ProcessConsensus(msg)
+            log.Println("node leader finishes process consensus for round 2")
         }
     }
 }
 
 func (n *Node) runAsMember() {
-    n.address
+    member1 := consensus.NewMember(store.GetLeader(), store.GetPrvKey())
+    log.Println("node member is going to process consensus for round 1")
+    bytes := member1.ProcessConsensus()
+    log.Println("node member finishes consensus for round 1")
+    block := &bean.Block{}
+    n.wg = &sync.WaitGroup{}
+    n.wg.Add(1)
+    if proto.Unmarshal(bytes, block) != nil {
+        member2 := consensus.NewMember(store.GetLeader(), store.GetPrvKey())
+        log.Println("node member is going to process consensus for round 2")
+        member2.ProcessConsensus()
+        log.Println("node member finishes consensus for round 2")
+    }
+    log.Println("node member is going to wait")
+    n.wg.Wait()
+    log.Println("node member finishes wait")
 }
 
 func (n *Node) runAsOther() {
@@ -70,5 +96,6 @@ func (n *Node) runAsOther() {
 }
 
 func (n *Node) processBlock(block *bean.Block) {
-
+    log.Println("Receive ", *block)
+    n.wg.Done()
 }
