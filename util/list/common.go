6358b8e24573fe6827e5012db760527f0011ca44
Eric Chao <ericchao@drep.org>
2018-09-10 17:38:54 +0800

    Finish the test for linked list

--- util/list/common.go
+++ util/list/common.go
@@ -5,27 +5,9 @@ type node struct {
     prev, next *node
 }
 
-type removable interface {
-    remove(node *node)
+type Iterator interface {
+    HasNext() bool
+    Next() interface{}
+    Remove()
 }
 
-type Iterator struct {
-    l removable
-    n *node
-}
-
-func (i *Iterator) hasNext() bool {
-    return i.n != nil
-}
-
-func (i *Iterator) next() interface{} {
-    t := i.n
-    i.n = i.n.next
-    return t.value
-}
-
-func (i *Iterator) remove() {
-    t := i.n
-    i.l.remove(t)
-    i.n = i.n.next
-}
