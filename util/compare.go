5c042f266a94e556df34c7b20dcbfce0c73589b1
Eric Chao <ericchao@drep.org>
2018-09-10 21:21:43 +0800

    Add transaction process

--- util/compare.go
+++ util/compare.go
@@ -1,27 +1,5 @@
 package util
 
-import "bytes"
-
-func Contains(a []byte, b [][]byte) bool {
-    length := len(b)
-    for i := 0; i < length; i++ {
-        if bytes.Equal(a, b[i]) {
-            return true
-        }
-    }
-    return false
-}
-
-func Subset(a [][]byte, b [][]byte) bool {
-    length := len(a)
-    for i := 0; i < length; i++ {
-        if !Contains(a[i], b) {
-            return false
-        }
-    }
-    return true
-}
-
 func SliceEqual(a []interface{}, b []interface{}, cp func(interface{}, interface{})bool) bool {
     if len(a) != len(b) {
         return false
