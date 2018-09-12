49c698bfa37f32a4a7ec81435b1fa216ade146e2
Eric Chao <ericchao@drep.org>
2018-09-12 23:33:42 +0800

    ...

--- processor/node_processor.go
+++ processor/node_processor.go
@@ -3,24 +3,23 @@ package processor
 import (
     "BlockChainTest/bean"
     "BlockChainTest/node"
+    "BlockChainTest/store"
 )
 
-type NewComerProcessor struct {
-}
+type NewComerProcessor struct {}
 
 func (p *NewComerProcessor) process(msg interface{}) {
     if newcomer, ok := msg.(*bean.Newcomer); ok {
-        if bean.IsAdmin {
-            node.GetNode().ProcessNewComers(newcomer)
+        if store.IsAdmin {
+            node.GetNode().ProcessNewComer(newcomer)
         }
     }
 }
 
-type PeersProcessor struct {
-}
+type PeersProcessor struct {}
 
 func (p *PeersProcessor) process(msg interface{}) {
     if listOfPeer, ok := msg.(*bean.ListOfPeer); ok {
-        node.GetNode()
+        node.GetNode().ProcessPeers(listOfPeer)
     }
 }
--- store/config.go
+++ store/config.go
@@ -1,9 +1,16 @@
 package store
 
-import "math/big"
+import (
+    "math/big"
+    "BlockChainTest/network"
+)
 
 var (
     BlockGasLimit  = big.NewInt(50)
     GasPrice = big.NewInt(5)
     TransferGas = big.NewInt(10)
+    // TODO
+    Admin = &network.Peer{IP: network.IP("192.168"), Port: 55555}
 )
+
+const IsAdmin = true
