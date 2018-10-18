6952847f8bf5b1721cbe1b0e6be15e20e6036a07
Eric Chao <ericchao@drep.org>
2018-10-18 15:53:51 +0800

    ttt

--- bean/node.pb.go
+++ bean/node.pb.go
@@ -6,7 +6,7 @@ package bean
 import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
-import mycrypto "mycrypto"
+import mycrypto "BlockChainTest/mycrypto"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
