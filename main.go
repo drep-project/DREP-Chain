7800d49e177303e734575c31aab11d42fb598d6a
hejiawei <jiaweihe@drep.org>
2018-11-14 20:30:46 +0800

    modify json body

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main()  {
 			p.Process(peer, t, msg)
 		}
 	}, store.GetPort())
-	//network.HttpStart()
+	network.HttpStart()
 	processor.GetInstance().Start()
 	node.GetNode().Start()
 	for {
