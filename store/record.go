1a76239b83ebcdd6be303f51ea7a3a45841a2dca
Eric Chao <ericchao@drep.org>
2018-12-19 14:22:22 +0800

    modify the l

--- store/record.go
+++ store/record.go
@@ -1,15 +1,23 @@
 package store
 
+import "sync"
+
 var (
     forwardedTrans = make(map[string]bool)
+    transLock sync.Mutex
     forwardedBlocks = make(map[string]bool)
+    blockLock sync.Mutex
 )
 
 func ForwardTransaction(id string) {
+    transLock.Lock()
+    defer transLock.Unlock()
     forwardedTrans[id] = true
 }
 
 func ForwardedTransaction(id string) bool {
+    transLock.Lock()
+    defer transLock.Unlock()
     v, exists := forwardedTrans[id]
     //TODO
     //fatal error: concurrent map read and map write
@@ -35,11 +43,15 @@ func ForwardedTransaction(id string) bool {
 }
 
 func ForwardBlock(id string) {
+    blockLock.Lock()
+    defer blockLock.Unlock()
     forwardedBlocks[id] = true
 }
 
 func ForwardedBlock(id string) bool {
     // TODO first check db and second check the pool
+    blockLock.Lock()
+    defer blockLock.Unlock()
     v, exists := forwardedBlocks[id]
     return v && exists
 }
