bdfd327dddeedb9ff1a9801f0b5d474abca9e175
xiezhenjian <xiezhenjian92@126.com>
2018-12-19 15:02:05 +0800

    del fmt

--- log/root.go
+++ log/root.go
@@ -2,7 +2,6 @@ package log
 
 import (
 	"os"
-	"fmt"
 )
 
 var (
@@ -42,8 +41,8 @@ func Debug(msg string, ctx ...interface{}) {
 
 // Info is a convenient alias for Root().Info
 func Info(msg string, ctx ...interface{}) {
-	fmt.Println("msg: ", msg)
-	fmt.Println("ctx: ", ctx)
+	//fmt.Println("msg: ", msg)
+	//fmt.Println("ctx: ", ctx)
 	//root.write(msg, LvlInfo, ctx, skipLevel)
 }
 
