82ea1bfe497719cbb301fc920f15a9992a8f6629
xiezhenjian <xiezhenjian92@126.com>
2019-02-14 14:54:37 +0800

    db_new

--- db_new/common.go
+++ db_new/common.go
@@ -14,13 +14,15 @@ func hex2Bytes(key string) []byte {
     return b
 }
 
-func getTrieKey(key []byte) string {
+func commonKey2TrieKey(key []byte) string {
     return bytes2Hex(mycrypto.Hash256(key, []byte("trie_key")))
 }
 
-func getTrieValue(value []byte) []byte {
+func commonValue2TrieValue(value []byte) []byte {
     return mycrypto.Hash256(value, []byte("trie_value"))
 }
+
+func getPasserBy
 
 func getChildKey(key []byte, child string) []byte {
     return mycrypto.Hash256(key, []byte("child"), []byte(child))
