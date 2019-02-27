2a1fac66be5ae2ca615529de7e41f945f35acfaf
unknown <xingwuwang@drep.org>
2019-02-27 19:55:34 +0800

    modify sync block logic

--- chain/service/syncchain.go
+++ chain/service/syncchain.go
@@ -8,18 +8,18 @@ import (
 
 func (chainService *ChainService) fetchBlocks() {
 	go func() {
-		for{
+		for {
 			chainService.P2pServer.Broadcast(&chainTypes.ReqPeerState{
-				Height:chainService.DatabaseService.GetMaxHeight(),
+				Height: chainService.DatabaseService.GetMaxHeight(),
 			})
-			time.Sleep(time.Second*5)
+			time.Sleep(time.Second * 5)
 			peer, state := chainService.GetBestPeer()
-			if peer == nil || state == nil || state.Height<1{
+			if peer == nil || state == nil || state.Height < 1 {
 				continue
 			}
 			if state.Height > chainService.DatabaseService.GetMaxHeight() {
-				req := &chainTypes.BlockReq{Height:chainService.DatabaseService.GetMaxHeight()}
-				chainService.P2pServer.Send(peer,req)
+				req := &chainTypes.BlockReq{Height: chainService.DatabaseService.GetMaxHeight()}
+				chainService.P2pServer.Send(peer, req)
 			}
 		}
 	}()
@@ -29,7 +29,7 @@ func (chainService *ChainService) handlePeerState(peer *p2pTypes.Peer, peerState
 	//get bestpeers
 	if _, ok := chainService.peerStateMap[string(peer.PubKey.Serialize())]; ok {
 		chainService.peerStateMap[string(peer.PubKey.Serialize())].Height = peerState.Height
-	}else{
+	} else {
 		chainService.peerStateMap[string(peer.PubKey.Serialize())] = peerState
 	}
 }
@@ -38,34 +38,34 @@ func (chainService *ChainService) handleReqPeerState(peer *p2pTypes.Peer, peerSt
 
 	if _, ok := chainService.peerStateMap[string(peer.PubKey.Serialize())]; ok {
 		chainService.peerStateMap[string(peer.PubKey.Serialize())].Height = peerState.Height
-	}else{
-		chainService.peerStateMap[string(peer.PubKey.Serialize())] =  &chainTypes.PeerState{Height : peerState.Height}
+	} else {
+		chainService.peerStateMap[string(peer.PubKey.Serialize())] = &chainTypes.PeerState{Height: peerState.Height}
 	}
 
 	chainService.P2pServer.SendAsync(peer, &chainTypes.PeerState{
-		Height : chainService.DatabaseService.GetMaxHeight(),
+		Height: chainService.DatabaseService.GetMaxHeight(),
 	})
 }
 
-func (chainService *ChainService) GetBestPeer() (*p2pTypes.Peer, *chainTypes.PeerState){
+func (chainService *ChainService) GetBestPeer() (*p2pTypes.Peer, *chainTypes.PeerState) {
 	peers := chainService.P2pServer.Peers()
 	if len(peers) == 0 {
 		return nil, nil
 	}
 	curPeer := peers[0]
 
-	for i:=1; i <len(peers);i++{
+	for i := 1; i < len(peers); i++ {
 		peerId := string(peers[i].PubKey.Serialize())
 		curPeerId := string(curPeer.PubKey.Serialize())
 		if _, ok := chainService.peerStateMap[peerId]; !ok {
-			chainService.peerStateMap[peerId] = &chainTypes.PeerState{Height : 0}
+			chainService.peerStateMap[peerId] = &chainTypes.PeerState{Height: 0}
 		}
 		if _, ok := chainService.peerStateMap[curPeerId]; !ok {
-			chainService.peerStateMap[curPeerId] = &chainTypes.PeerState{Height : 0}
+			chainService.peerStateMap[curPeerId] = &chainTypes.PeerState{Height: 0}
 		}
 		if chainService.peerStateMap[peerId].Height > chainService.peerStateMap[curPeerId].Height {
 			curPeer = peers[i]
 		}
 	}
-	return curPeer,  chainService.peerStateMap[string(curPeer.PubKey.Serialize())]
+	return curPeer, chainService.peerStateMap[string(curPeer.PubKey.Serialize())]
 }
