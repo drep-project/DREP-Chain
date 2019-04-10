240584c5f1cd0bcbf2e2ec74ca43a433f8fe0519
xingwuwang <47589513+xingwuwang@users.noreply.github.com>
2019-04-10 16:00:36 +0800

    Update interface.go
    method not be used

--- network/service/interface.go
+++ network/service/interface.go
@@ -13,7 +13,7 @@ type P2P interface {
 	Broadcast(msg interface{})
 	Peers() []*p2pTypes.Peer
 	GetPeer(ip string) *p2pTypes.Peer
-	GetRouter() *p2pTypes.MessageRouter
+	//GetRouter() *p2pTypes.MessageRouter
 	AddPeer(addr string) error
 	RemovePeer(addr string)
 	SetIdle(peer *p2pTypes.Peer, idle bool)
