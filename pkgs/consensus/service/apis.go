2a08ec202ea6be9532d0059a47d1bb3eae2226bd
hunjixin <1084400399@qq.com>
2019-06-25 14:24:32 +0800

    fix changeWaitTime api

--- pkgs/consensus/service/apis.go
+++ pkgs/consensus/service/apis.go
@@ -29,3 +29,18 @@ func (consensusApi *ConsensusApi) Minning() bool {
 	}
 }
 
+/*
+	 name: changeWaitTime
+	 usage: 修改leader等待时间 (ms)
+	 params:
+		1.等待时间(ms)
+	 return: 私钥
+	 example:
+		curl http://localhost:15645 -X POST --data '{"jsonrpc":"2.0","method":"consensus_changeWaitTime","params":[100000], "id": 3}' -H "Content-Type:application/json"
+
+	response:
+		 {"jsonrpc":"2.0","id":3,"result":null}
+*/
+func (consensusApi *ConsensusApi) ChangeWaitTime(waitTime int) {
+	consensusApi.consensusService.ChangeWaitTime(int64(waitTime))
+}
