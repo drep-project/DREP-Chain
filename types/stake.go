b2c8d813ce5a03f70ce7e734a971f7a19fc9060c
drep <wxw@drep.org>
2019-09-20 15:45:41 +0800

    split storage

--- types/stake.go
+++ types/stake.go
@@ -1,1 +1,14 @@
 package types
+
+import (
+	"github.com/drep-project/drep-chain/crypto"
+	"math/big"
+)
+
+type StakeStorage struct {
+	ReceivedVoteCredit map[crypto.CommonAddress]big.Int //Trust given by oneself and others
+	//SentVoteCredit     map[crypto.CommonAddress]big.Int //Vote for Trust to Address
+	CancelVoteCredit map[*big.Int]big.Int //撤销给与别人的信任数据存放于此；等待一段时间或者高度后，value对应的balance加入到Balance中。key是撤销时交易所在的高度
+
+	//LockBalance        map[crypto.CommonAddress]big.Int //智能合约中即可
+}
