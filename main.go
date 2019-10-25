d670712cdded0d9230faa18c8dd9da6dbeec9ecf
hunjixin <1084400399@qq.com>
2019-10-25 15:19:23 +0800

    fix check node host

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	crand "crypto/rand"
+	"encoding/json"
 	"fmt"
+	"github.com/drep-project/drep-chain/common"
 	"github.com/drep-project/drep-chain/crypto/secp256k1"
+	"github.com/drep-project/drep-chain/types"
 	"github.com/meling/urs"
 	"math/rand"
 	"time"
@@ -21,6 +24,16 @@ func FromECDSAPub(pub *ecdsa.PublicKey) []byte {
 type AAA string
 
 func main() {
+	_,pk := secp256k1.PrivKeyFromScalar(common.MustDecode("0x29936977e507d150e2bb699042e605cdcebb59b83db372f8895b814904be8179"))
+	dddd, _ := json.Marshal(pk)
+	fmt.Println(string(dddd))
+	fmt.Println(common.Encode(pk.Serialize()))
+//	curl -H "Content-Type: application/json" -X post --data '{"jsonrpc":"2.0","method":"account_candidateCredit","params":["0x300fc5a14e578be28c64627c0e7e321771c58cd4","0x3641100","0x110","0x30000","{\"Pubkey\":\"0x03ad000bc9a4a29c11227d6b5ee05076b594c1c22567cdd85fbb8222e6a715924e\",\"Node\":\"enode://da388eb91ff35bc840b30a0adc771507cb5e933dff0818cf687644341e477e7e@192.168.147.134:55555\"}"],"id":1}' http://127.0.0.1:15645
+
+str := "{\"Pubkey\":\"0x03ad000bc9a4a29c11227d6b5ee05076b594c1c22567cdd85fbb8222e6a715924e\",\"Node\":\"enode://da388eb91ff35bc840b30a0adc771507cb5e933dff0818cf687644341e477e7e@192.168.147.134:55555\"}"
+	ddd := &types.CandidateData{}
+	ec := 	ddd.Unmarshal([]byte(str))
+	fmt.Println(ec)
 	eatList := []string{"大娘水饺", "沙县小吃", "沙县隔壁", "河南面馆"}
 	index := rand.Intn(int(time.Now().Unix()%int64(len(eatList))) + 1)
 	fmt.Println(eatList[index])
