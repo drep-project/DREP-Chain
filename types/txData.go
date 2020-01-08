00610fc07c8a1047a0457c308d659919e2681fd4
drep <wxw@drep.org>
2020-01-08 15:15:15 +0800

    check host

--- types/txData.go
+++ types/txData.go
@@ -4,16 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/drep-project/DREP-Chain/crypto/secp256k1"
-	"github.com/drep-project/DREP-Chain/network/p2p/enode"
+	"net"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type P2pNode struct {
-
 }
+
 //候选节点数据部分信息
 type CandidateData struct {
-	Pubkey *secp256k1.PublicKey       //出块节点的pubkey
-	Node  string //出块节点的地址
+	Pubkey *secp256k1.PublicKey //出块节点的pubkey
+	Node   string               //出块节点的地址
 }
 
 func (cd CandidateData) check() error {
@@ -43,6 +46,32 @@ func (cd *CandidateData) Unmarshal(data []byte) error {
 }
 
 func hostAddrCheck(addr string) bool {
-	n := enode.Node{}
-	return n.UnmarshalText([]byte(addr)) == nil
+	items := strings.Split(addr, ":")
+	if items == nil || len(items) != 2 {
+		return false
+	}
+
+	a := net.ParseIP(items[0])
+	if a == nil {
+		return false
+	}
+
+	match, err := regexp.MatchString("^[0-9]*$", items[1])
+	if err != nil {
+		return false
+	}
+
+	i, err := strconv.Atoi(items[1])
+	if err != nil {
+		return false
+	}
+	if i < 0 || i > 65535 {
+		return false
+	}
+
+	if match == false {
+		return false
+	}
+
+	return true
 }
