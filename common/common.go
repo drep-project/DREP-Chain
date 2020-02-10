4c4b4933309a09f94c2f1f8c730b1318b7340e62
drep <wxw@drep.org>
2020-02-10 10:31:08 +0800

    modify ttl255

--- common/common.go
+++ common/common.go
@@ -4,6 +4,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha512"
 	"github.com/drep-project/DREP-Chain/crypto/sha3"
+	"math/big"
 	"math/rand"
 	"net"
 	"os"
@@ -11,6 +12,16 @@ import (
 )
 
 var Version int32 = 1
+
+var (
+	Big1   = big.NewInt(1)
+	Big2   = big.NewInt(2)
+	Big3   = big.NewInt(3)
+	Big0   = big.NewInt(0)
+	Big32  = big.NewInt(32)
+	Big256 = big.NewInt(256)
+	Big257 = big.NewInt(257)
+)
 
 func HmAC(message, key []byte) []byte {
 	h := hmac.New(sha512.New, key)
