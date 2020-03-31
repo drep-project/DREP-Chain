5cd9c34d87555fa7245e643b610b8148ebe534c9
drep <wxw@drep.org>
2020-03-31 09:41:17 +0800

    del stop signal

--- app/signal.go
+++ app/signal.go
@@ -8,11 +8,11 @@ import (
 
 func exitSignal(ch chan struct{}) {
 	c := make(chan os.Signal)
-	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGSEGV, syscall.SIGTSTP)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGSEGV)
 	go func() {
 		for s := range c {
 			switch s {
-			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGSEGV, syscall.SIGTSTP:
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGSEGV:
 				ch <- struct{}{}
 			default:
 			}
