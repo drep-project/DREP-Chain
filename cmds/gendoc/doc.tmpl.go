5f7d440322cb3c76d1bdc8c44d0dc857bddc0fbc
alvin-kun <1196859010@qq.com>
2020-06-24 19:48:43 +0800

    doc.templ change

--- cmds/gendoc/doc.tmpl.go
+++ cmds/gendoc/doc.tmpl.go
@@ -1,29 +1,36 @@
 package main
 
 const doctmpl = `
-#  ` + "`" + `JSON-RPC` + "`" + `RPC interface
+#  RPC interface
 ## {{range .}}{{ .StructDocStr | trim | html}}
 {{ .FuncDocStr | html}}{{end}}`
 
 const methodtmpl = `{{range $index2, $method := .}}
-### {{inc $index2}}. {{$method.Prefix}}_{{$method.Name}}
-#### usage：{{ with (index .Tokens "usage:") }}{{ .Str }}{{ end }}
-> params：
+## {{inc $index2}}. {{$method.Prefix}}_{{$method.Name}}
+<font size=5> Usage：{{ with (index .Tokens "usage:") }}{{ .Str }}{{ end }} </font>  
+
+<font size=5> Params：</font>  
+
 {{ with (index $method.Tokens "params:") }}{{range $index, $params := .Params}} {{inc $index}}. {{$params}}
 {{end}}{{ end }}
-#### return：{{ with (index $method.Tokens "return:") }}{{.Str | html}}{{ end }}
+<font size=5> Return：{{ with (index $method.Tokens "return:") }}{{.Str | html}}{{ end }} </font>
 
-#### example
+<font size=5> Example: </font>   
 
+<font size=4> shell: </font>
 ` + "```" + `shell
 {{ with (index $method.Tokens "example:") }}{{.Str | html}}{{ end }}
 ` + "```" + `
+<font size=4> cli: </font>
+` + "```" + `cli
+drepClient 127.0.0.1:10085 {{$method.Prefix}}_{{$method.Name}} 3
+` + "```" + `
 
-##### response：
+<font size=5> Response：</font>
 
 ` + "```" + `json
 {{ with (index $method.Tokens "response:") }}{{ .Str | html }}{{ end }}
-` + "````" + `
+` + "```" + `
 
 {{end}}`
 
