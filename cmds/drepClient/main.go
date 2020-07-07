b7a763c9c4e056d7cde61bf68917e968464b568e
liamwk <liamwang@drep.org>
2020-07-07 11:54:06 +0800

    add genaccount cli

--- cmds/drepClient/main.go
+++ cmds/drepClient/main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"github.com/drep-project/DREP-Chain/chain"
 	"github.com/drep-project/DREP-Chain/common/hexutil"
+	"github.com/drep-project/DREP-Chain/crypto"
 	"github.com/drep-project/DREP-Chain/crypto/secp256k1"
+	"github.com/drep-project/DREP-Chain/network/p2p/enode"
+	p2pTypes "github.com/drep-project/DREP-Chain/network/types"
+	"github.com/drep-project/DREP-Chain/params"
+	accountComponent "github.com/drep-project/DREP-Chain/pkgs/accounts/component"
 	"github.com/drep-project/DREP-Chain/pkgs/accounts/service"
+	accountTypes "github.com/drep-project/DREP-Chain/pkgs/accounts/types"
+	chainIndexerTypes "github.com/drep-project/DREP-Chain/pkgs/chain_indexer"
+	cservice "github.com/drep-project/DREP-Chain/pkgs/consensus/service"
+	"github.com/drep-project/DREP-Chain/pkgs/consensus/service/bft"
+	filterTypes "github.com/drep-project/DREP-Chain/pkgs/filter"
+	"github.com/drep-project/DREP-Chain/pkgs/log"
 	"github.com/drep-project/DREP-Chain/pkgs/trace"
 	"math/big"
 	"net/url"
 	"os"
+	path2 "path"
 	"strconv"
 	"sync"
 	"time"
@@ -872,6 +886,163 @@ func getPoolTransactions(args cli.Args, client *rpc.Client, ctx context.Context)
 	}
 }
 
+type Genesis struct {
+	Preminer []*chain.Preminer
+	Miners   []types.CandidateData
+}
+
+func getCurPath() string {
+	dir, _ := os.Getwd()
+	return dir
+}
+
 func genaccount(args cli.Args, client *rpc.Client, ctx context.Context)  {
 
+	if err := argsJudge(args, 3); err != nil {
+		fmt.Println("genaccount cli need name and password params")
+		fmt.Println(err.Error())
+		return
+	}
+
+	path := getCurPath()
+	// args[1] define name of directory
+	userDir := path2.Join(path, args[1])
+	// args[2] define password of keystore
+	password := args[2]
+
+	var genesis Genesis
+	var staticConfigs = [4]string{"enode://f57881c48aaccf97485c2b65b421bfeda22cc3b427c44be7607b122fc1688abb@172.104.123.143:10086", "enode://9d25d161ae4b676e2df55accca93c3137df3166326d04420ffbdf66e887bd494@172.104.116.219:10086", "enode://bc7ca1b57175f2d5c85da73d367408529468a034b97d083aaecf88196090e245@172.105.103.59:10086", "enode://0ebd0422ca32d70292be128342f9e5ca32ab3cef28dc32cc332169e578e7b4f5@109.74.203.199:10086"}
+	var genesisConfig = "{\"Preminer\": [{\"Addr\": \"0xfb6711cbafbd5e75c612d69db9025f7eb5096d46\", \"Value\": 10000000000000000000000000000}],\"Miners\": [{\"Pubkey\": \"0x0305bfc35e079ca7ae7ad6fcf246f8ed0247d0fdbda0b7daa10f2b2f3a88e9fd8a\",\"Node\": \"enode://f57881c48aaccf97485c2b65b421bfeda22cc3b427c44be7607b122fc1688abb@172.104.123.143:10086\"},{\"Pubkey\": \"0x0354d5d560039693ec4dedb416354bdaa7aa70823fd5fabf56ceeb76386efc5670\",\"Node\": \"enode://9d25d161ae4b676e2df55accca93c3137df3166326d04420ffbdf66e887bd494@172.104.116.219:10086\"},{\"Pubkey\": \"0x031283a05887e8b291fd909a6302cdd84b0b50bbc0ae5da8ce186435a1c6b8d10a\",\"Node\": \"enode://bc7ca1b57175f2d5c85da73d367408529468a034b97d083aaecf88196090e245@172.105.103.59:10086\"},{\"Pubkey\": \"0x0349028fc4ce0ed5b6d496fcbd12755779954c1a17e74dfb283190e14ee5ea3170\",\"Node\": \"enode://0ebd0422ca32d70292be128342f9e5ca32ab3cef28dc32cc332169e578e7b4f5@109.74.203.199:10086\"},{\"Pubkey\": \"0x03902b01673cc0ffad466f68a7a1494c9da1a80fb63dae8c9d18dfc8f9aeff1eba\",\"Node\": \"enode://c01ff36a9914f781a058c77e98f09eedd5ad7e2e575c7e6c2d6cc86d859693f2@139.162.161.8:10086\"},{\"Pubkey\": \"0x028b2388f1cd0b8056a3ef41a7559952a0abe40ce8d93f586c7aa23b02f841ed23\",\"Node\": \"enode://bb7925d62126c1058f7ad951e862aa4fc0aaaa4dc7cae2fcac3b86aee316bc02@172.104.178.138:10086\"},{\"Pubkey\": \"0x0284454961eb9c389bf292e1d9a1eb957e4b22249771ce942a9eda1768fe607b32\",\"Node\": \"enode://9863364f265843bbb2d7810c9495e83b04cc541bedc8ae44ca63a2c5ec9d1b75@172.105.182.178:10086\"},{\"Pubkey\": \"0x02e42e247ae1f9a737aaa974f83dc8ca2e96c9e9a635e7e4d124df633a886d5e0e\",\"Node\": \"enode://7523f570a35d58841440a792b51ae0608a1c7f387d7f0899c59720a5f851604a@172.105.42.46:10086\"},{\"Pubkey\": \"0x036b07d786bd104b83069049be0f918c9b1505f5a90ab645837794a20c63f2b6f1\",\"Node\": \"enode://179838dc7bdb7285f82ec8e15515e09208a7d7452898dd4b978df88b694402d3@96.126.122.90:10086\"},{\"Pubkey\": \"0x0299e438ccbca1a492dfa04ab3189b687f6311fc99cd9f6112b23b4433e4c758ec\",\"Node\": \"enode://ab789802b04057655247b51fb58f48643a7a4ae883a786ee4871b67c6e57250f@45.79.252.45:10086\"},{\"Pubkey\": \"0x025d404222e2f67a77963ae7cd846bc95174e0150d47c61665b5a280fbef989c64\",\"Node\": \"enode://2a87c3c98b416f22d11e8952b36c220942461ccd69ecdbc54aef1b0d90238da4@45.56.95.177:10086\"},{\"Pubkey\": \"0x02ea32b18f6a37b23ebccca1ed6ae4f513da764f85f3a37234c18cad627e820dc9\",\"Node\": \"enode://8cec1501ea4bb26b6404809e4e3eef53149b490ec7d378085dd7a24b9e6d1211@45.33.89.236:10086\"},{\"Pubkey\": \"0x0398586c5d012e677ef9eb74785de6aa69e38c13e17e45071ec91c9e32bdc64848\",\"Node\": \"enode://5a764d497a55e42ea33a833770f63e2ba8f6200da824db32ef2bcc937af88c44@176.58.112.109:10086\"},{\"Pubkey\": \"0x03ce8088cd8ab8fa7cfd7ecd71015bf525d9b071b0090889e399bc021e10496770\",\"Node\": \"enode://956f3f79214db8065298e8447f8aa14ca611c02bb886a1f1e0be92eb74ad8984@172.104.179.82:10086\"},{\"Pubkey\": \"0x022d06d1131b740ee6324d35c4096ce5dc54a90c0b962b795fd46fa371bc734e82\",\"Node\": \"enode://8ecedb07fcb27642c1560e000c79fa80a4e185001a40030fb50e698ff008a98d@139.162.188.229:10086\"},{\"Pubkey\": \"0x0306c3c718e9f9fad21a63c27e1527c599ec54611e17d6b90fa9a81dc7b6588624\",\"Node\": \"enode://f2055090bb60b42c91272ce00f671c0048c46e065d489910341c101d4bb94af3@23.239.16.64:10086\"},{\"Pubkey\": \"0x0373d68e621f5063875c9b4647cb7b990d96f59fb3769cbb892e462975937acb48\",\"Node\": \"enode://772c23d1de1ac72451c17da61889b5c211127393916f93a8671c202aad2ef9df@45.56.124.251:10086\"},{\"Pubkey\": \"0x02b6d93dddb0cb702266f55b7982067d4b6f5587af0273b660db8ac590d405733f\",\"Node\": \"enode://d8eb92d787384442e40476fa1d4c564facca3667298566b3950554ce4cc0036f@172.104.106.112:10086\"},{\"Pubkey\": \"0x02da50f6372568a6f8dc9d05a65659dc7d5db7db5c1c9bdc4369c70ceb19200b28\",\"Node\": \"enode://6890bf120d755a498a301e9cbbcbabba0c4c6628133c35e5624eb85b5e1f09ae@172.105.175.246:10086\"},{\"Pubkey\": \"0x03d94c1a566bf73783dbe21bfb4fb7b3e673de16a94f5b616c32bd4d44a2bf30c9\",\"Node\": \"enode://f0f207d9199a246871759b29424358bc23bb9d2e3730dc3b151c9f9abdddd4be@172.105.206.6:10086\"},{\"Pubkey\": \"0x03c1586860d81359a51655dfe03663beb4db51ec5373f7b39a525561498dfe31b6\",\"Node\": \"enode://5969d83137afd2ea7ed06419009ea52f86213094b46c6444468880f45659fedd@139.162.30.122:10086\"}]}"
+
+	err := json.Unmarshal([]byte(genesisConfig), &genesis)
+	if err != nil {
+		fmt.Println("unmarshal genesis info err")
+		return
+	}
+
+	trustNodes := []*enode.Node{}
+	for i := 0; i < len(staticConfigs); i++ {
+		node, err := enode.ParseV4(staticConfigs[i])
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		trustNodes = append(trustNodes, node)
+	}
+
+	node := getAccount(args[1])
+	standbyKey := node.PrivateKey
+
+	logConfig := log.LogConfig{}
+	logConfig.LogLevel = 4
+
+	rpcConfig := rpc.RpcConfig{}
+	rpcConfig.IPCEnabled = true
+	rpcConfig.HTTPEnabled = true
+
+	p2pConfig := p2pTypes.P2pConfig{}
+	p2pConfig.MaxPeers = 25
+	p2pConfig.NoDiscovery = false
+	p2pConfig.DiscoveryV5 = true
+	p2pConfig.Name = "drepnode"
+	p2pConfig.StaticNodes = trustNodes
+
+	consensusConfig := &cservice.ConsensusConfig{}
+	consensusConfig.ConsensusMode = "bft"
+	consensusConfig.Bft = &bft.BftConfig{
+		MyPk:           nil,
+		StartMiner:     true,
+		BlockInterval:  15,
+		ProducerNum:    21,
+		ChangeInterval: 100,
+	}
+
+	chainConfig := chain.ChainConfig{}
+	chainConfig.ChainId = 0
+	chainConfig.GenesisAddr = params.HoleAddress
+
+	chainIndexerConfig := chainIndexerTypes.ChainIndexerConfig{}
+	chainIndexerConfig.Enable = true
+	chainIndexerConfig.SectionSize = 4096
+	chainIndexerConfig.ConfirmsReq = 256
+	chainIndexerConfig.Throttling = 100 * time.Millisecond
+
+	filterConfig := filterTypes.FilterConfig{}
+	filterConfig.Enable = true
+
+	consensusConfig.Bft.MyPk = (*secp256k1.PublicKey)(&standbyKey.PublicKey)
+
+	p2pConfig.ListenAddr = "0.0.0.0:10086"
+	chainConfig.RemotePort = 10087
+
+	os.MkdirAll(userDir, os.ModeDir|os.ModePerm)
+	keyStorePath := path2.Join(userDir, "keystore")
+
+	store := accountComponent.NewFileStore(keyStorePath)
+	store.StoreKey(node, password)
+
+	walletConfig := accountTypes.Config{}
+	walletConfig.Enable = true
+
+	cfgPath := path2.Join(userDir, "config.json")
+	fs, _ := os.Create(cfgPath)
+	offset := int64(0)
+	fs.WriteAt([]byte("{\n"), offset)
+	offset = int64(2)
+
+	offset = writePhase(fs, "log", logConfig, offset)
+	offset = writePhase(fs, "rpc", rpcConfig, offset)
+	offset = writePhase(fs, "consensus", consensusConfig, offset)
+	offset = writePhase(fs, "p2p", p2pConfig, offset)
+	offset = writePhase(fs, "chain", chainConfig, offset)
+	offset = writePhase(fs, "accounts", walletConfig, offset)
+	offset = writePhase(fs, "chain_indexer", chainIndexerConfig, offset)
+	offset = writePhase(fs, "filter", filterConfig, offset)
+	offset = writePhase(fs, "genesis",genesis, offset)
+	err = fs.Truncate(offset - 2)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	_, err = fs.WriteAt([]byte("\n}"), offset-2)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	return
+}
+
+func writePhase(fs *os.File, name string, config interface{}, offset int64) int64 {
+	bytes, _ := json.MarshalIndent(config, "	", "      ")
+	bytes = append([]byte("	\""+name+"\" : "), bytes...)
+	fs.WriteAt(bytes, offset)
+	offset += int64(len(bytes))
+
+	fs.WriteAt([]byte(",\n"), offset)
+	offset += 2
+	return offset
+}
+
+func getAccount(name string) *types.Node {
+	node := RandomNode([]byte(name))
+	return node
+}
+
+func RandomNode(seed []byte) *types.Node {
+	var (
+		prvKey    *secp256k1.PrivateKey
+		chainCode []byte
+	)
+
+	prvKey, _ = crypto.GenerateKey(rand.Reader)
+	chainCode = append(seed, []byte(types.DrepMark)...)
+	chainCode = common.HmAC(chainCode, prvKey.PubKey().Serialize())
+
+	addr := crypto.PubkeyToAddress(prvKey.PubKey())
+	return &types.Node{
+		PrivateKey: prvKey,
+		Address:    &addr,
+		ChainId:    0,
+		ChainCode:  chainCode,
+	}
 }
